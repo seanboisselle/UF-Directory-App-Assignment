42dc72c8a52c5c59557eaeb97a33bbe67279ccca
Sean Boisselle <sboisselle@ufl.edu>
2017-09-10 19:02:30 -0400

    Update listingController.js

--- listingController.js
+++ listingController.js
@@ -1,14 +1,48 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
+    $scope.code = "";
+    $scope.name = "";
+    $scope.address = "";
+    $scope.latitude = "";
+    $scope.longitude = "";
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
-     */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      var addListing = {
+        "code" : $scope.code,
+        "name" : $scope.name,
+        "address" : $scope.address,
+          "coordinates" : {
+              "latitude" : $scope.latitude,
+              "longitude" : $scope.longitude
+          }
+      };
+        $scope.listings.push(addListing);
+        $scope.code = "";
+        $scope.name = "";
+        $scope.address = "";
+        $scope.latitude = "";
+        $scope.longitude = "";
+    };
+
+    $scope.deleteListing = function(index) {
+          index = $scope.listings.indexOf(index)
+          $scope.listings.splice(index, 1);
+    };
+
+    $scope.showDetails = function(index) {
+      $scope.info = {
+        "latitude" : "",
+        "longitude" : "",
+        "address" : ""
+      };
+
+      $scope.info = {
+        "latitude" : "Latitude: " + index.coordinates.latitude,
+        "longitude" : "Longitude:  " + index.coordinates.longitude,
+        "address" : "Address:  " + index.address
+      };
+    };
   }
 ]);
